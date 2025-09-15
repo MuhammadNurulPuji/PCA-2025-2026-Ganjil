b609ef4f0b8513d16223c347790d7a39c685414e
Muhammad Nurul Puji <muhammadpuji.its@gmail.com>
2025-09-15 13:41:19 +0700

    Add Multi Led

--- Week02/Multi_LED_02/Multi_LED_02.ino
+++ Week02/Multi_LED_02/Multi_LED_02.ino
@@ -14,29 +14,35 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(0,HIGH);
   delay(500);
+  digitalWrite(0, LOW);
+
   digitalWrite(1,HIGH);
   delay(500);
+  digitalWrite(1, LOW);
+
   digitalWrite(2,HIGH);
   delay(500);
+  digitalWrite(2, LOW);
+
   digitalWrite(3,HIGH);
   delay(500);
+  digitalWrite(3, LOW);
+
   digitalWrite(4,HIGH);
   delay(500);
+  digitalWrite(4, LOW);
+
   digitalWrite(5,HIGH);
   delay(500);
+  digitalWrite(5, LOW);
+
   digitalWrite(6,HIGH);
   delay(500);
+  digitalWrite(6, LOW);
+
   digitalWrite(7,HIGH);
   delay(500);
-
-
-  digitalWrite(0, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
   digitalWrite(7, LOW);
+
   
 }
